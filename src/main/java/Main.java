2a5737dd2e52738352c7f3d28ba4a8de38fbe1a0
Vladislav Tretyakov <ididnothing86@gmail.com>
2022-10-02 20:55:49 +0600

    version2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
         Post post = new Post();
-        FormData birthday = new FormData();
+        post.birthday = new FormData();
 
         post.name = "Иван";
         post.patronymic = "Иванович";
         post.surname = "Иванов";
-        birthday.day = 15;
-        birthday.month = 8;
-        birthday.year = 1997;
         post.passport = "4444№444444";
         post.phone = "89994445566";
         post.subscription = true;
+        post.birthday.day = 15;
+        post.birthday.month = 12;
+        post.birthday.year = 1994;
     }
 }
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -6,4 +6,5 @@ public class Post {
     public String phone;
     public boolean subscription;
 
+    public FormData birthday;
 }
